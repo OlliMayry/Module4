46befc0fd181e45ad07bb09bef5f5bafe7a74455
OlliMayry <olli.mayry@gmail.com>
2022-10-30 17:35:54 +0200

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'newproject-3532b',
+    appId: '1:116325335458:web:afe070287efdf66ffd72fb',
+    storageBucket: 'newproject-3532b.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBSCmuGIn2IffXRZOOHqEjdzDReT6O-vKc',
+    authDomain: 'newproject-3532b.firebaseapp.com',
+    messagingSenderId: '116325335458',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'newproject-3532b',
+    appId: '1:116325335458:web:afe070287efdf66ffd72fb',
+    storageBucket: 'newproject-3532b.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBSCmuGIn2IffXRZOOHqEjdzDReT6O-vKc',
+    authDomain: 'newproject-3532b.firebaseapp.com',
+    messagingSenderId: '116325335458',
+  },
   production: false
 };
 
